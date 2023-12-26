6a38e3ebbff8b5a26bf5c12158feaf0c6a1998df
Olartbaraq <akanbi.ges201@gmail.com>
2023-12-26 22:27:28 +0100

    some site

--- redux/store.ts
+++ redux/store.ts
@@ -9,3 +9,6 @@ export const Store = configureStore({
 
 export type AppDispatch = typeof Store.dispatch;
 export type RootState = ReturnType<typeof Store.getState>;
+
+
+// https://blog.logrocket.com/using-typescript-redux-toolkit/
